1196eb828277527de4f423a7fe61bb47ca9c11f4
Artur <artur@imac-art.local>
2017-02-20 11:05:19 +0300

    Initial Commit

---
+++ SimpleTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTestProject
+//
+//  Created by Artur on 20.02.17.
+//  Copyright © 2017 Artur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
